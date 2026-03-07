e1625fe7b01a968090441b4d9ba1b26b8febf2ea
Antonio Baltic <antoniobaltic@icloud.com>
2026-03-07 08:34:22 +0100

    Run middleware on Node.js in Vercel

--- middleware.ts
+++ middleware.ts
@@ -60,6 +60,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
+  runtime: "nodejs",
   matcher: [
     "/((?!_next/static|_next/image|favicon.ico|images|fonts|api/webhooks).*)",
   ],
