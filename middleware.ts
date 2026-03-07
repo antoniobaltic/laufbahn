803c741883a7042472daa5d7e6ebbf604ac2d25d
Antonio Baltic <antoniobaltic@icloud.com>
2026-03-07 08:37:29 +0100

    Stabilize Vercel Next.js deployment

--- middleware.ts
+++ middleware.ts
@@ -60,7 +60,6 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  runtime: "nodejs",
   matcher: [
     "/((?!_next/static|_next/image|favicon.ico|images|fonts|api/webhooks).*)",
   ],
